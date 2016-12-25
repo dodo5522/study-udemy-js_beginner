21811461471b710a3882989b10a43a48b1ff0b00
Takashi Ando <dodo5522@gmail.com>
2016-12-26 00:20:21 +0900

    checkboxの状態をpropで取得、コピーする

--- 05-questionnaire/js/mine.js
+++ 05-questionnaire/js/mine.js
@@ -2,4 +2,9 @@ $(function() {
   $("#name").change(function() {
     var name = $("name").val();
   });
+
+  $("#ok").change(function() {
+    var checked = $("#ok").prop("checked");
+    $("#ok2").prop("checked", checked);
+  });
 });
