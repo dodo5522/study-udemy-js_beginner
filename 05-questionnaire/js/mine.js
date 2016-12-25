018176e290a333ddcf1dc742383f513694ab5180
Takashi Ando <dodo5522@gmail.com>
2016-12-26 00:38:01 +0900

    copyボタンクリックでコピー処理する

--- 05-questionnaire/js/mine.js
+++ 05-questionnaire/js/mine.js
@@ -1,10 +1,20 @@
 $(function() {
-  $("#name").change(function() {
-    var name = $("name").val();
-  });
+  var copyName = function() {
+    var name = $("#name").val();
+    $("#name2").val(name);
+  };
 
-  $("#ok").change(function() {
+  var copyOk = function() {
     var checked = $("#ok").prop("checked");
     $("#ok2").prop("checked", checked);
+  };
+
+  var copyAll = function() {
+    copyName();
+    copyOk();
+  };
+
+  $("#btnCopy").click(function() {
+    copyAll();
   });
 });
