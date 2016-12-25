e4f9aa16643268d6f2a48cf134dfdccd923f928e
Takashi Ando <dodo5522@gmail.com>
2016-12-26 07:21:05 +0900

    趣味checkboxの状態コピー処理を追加

--- 05-questionnaire/js/mine.js
+++ 05-questionnaire/js/mine.js
@@ -9,9 +9,24 @@ $(function() {
     $("#ok2").prop("checked", checked);
   };
 
+  var copyHobby = function() {
+    $("input[type=checkbox][name=hobby]").each(function() {
+      var srcId = "#" + $(this).prop("id");
+      var destId = "#" + $(this).prop("id") + "2";
+      var val = $(this).prop("checked");
+
+      console.log(val);
+      console.log(srcId);
+      console.log(destId);
+
+      $(destId).prop("checked", val);
+    });
+  };
+
   var copyAll = function() {
     copyName();
     copyOk();
+    copyHobby();
   };
 
   $("#btnCopy").click(function() {
